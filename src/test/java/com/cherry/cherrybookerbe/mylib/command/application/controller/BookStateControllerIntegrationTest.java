4dc911acfa17e08556acba5109dd969c71b5b384
penrose0214 <penrose0214@gmail.com>
2025-12-09 09:54:00 +0900

    update: minor fixes and comments added

--- src/test/java/com/cherry/cherrybookerbe/mylib/command/application/controller/BookStateControllerIntegrationTest.java
+++ src/test/java/com/cherry/cherrybookerbe/mylib/command/application/controller/BookStateControllerIntegrationTest.java
@@ -6,8 +6,8 @@ import com.cherry.cherrybookerbe.mylib.command.domain.repository.MyLibRepository
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
