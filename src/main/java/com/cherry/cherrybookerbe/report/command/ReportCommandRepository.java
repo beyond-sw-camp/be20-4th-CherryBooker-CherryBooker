0ef0e670689d54644b97e49390719df2ec70ef51
longlive13 <papiairplane@gmail.com>
2025-12-09 10:33:19 +0900

    chore : ReportCommandRepository JPA 설정 추가
    - JpaRepository 상속을 통한 신고 command 저장용 repository 구성
    - 신고 저장 및 단건 조회를 위한 기본 JPA 기능 사용 가능하도록 설정

--- src/main/java/com/cherry/cherrybookerbe/report/command/ReportCommandRepository.java
+++ src/main/java/com/cherry/cherrybookerbe/report/command/ReportCommandRepository.java
@@ -1,9 +1,9 @@
 package com.cherry.cherrybookerbe.report.command;
 
 import com.cherry.cherrybookerbe.report.domain.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 // 신고 저장
-// 스레드id 기준 신고 개수 count
 public interface ReportCommandRepository
         extends JpaRepository<Report, Long> {
 }
