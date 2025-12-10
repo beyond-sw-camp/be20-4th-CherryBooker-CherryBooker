ec3cff80ddd206275818b02613cd0379fe65c71e
dzmajin <qpalzm3415@gmail.com>
2025-12-10 15:07:00 +0900

    chore: 변수명 수정, 공백 삭제

--- src/main/java/com/cherry/cherrybookerbe/user/command/service/UserCommandServiceImpl.java
+++ src/main/java/com/cherry/cherrybookerbe/user/command/service/UserCommandServiceImpl.java
@@ -5,7 +5,6 @@ import com.cherry.cherrybookerbe.user.command.domain.entity.UserStatus;
 import com.cherry.cherrybookerbe.user.command.dto.request.UpdateNicknameRequest;
 import com.cherry.cherrybookerbe.user.command.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
